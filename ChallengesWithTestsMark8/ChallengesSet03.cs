6462a965261b3d279aec8e085510bb19bdbb0c59
MuscuarBean <email@example.com>
2023-11-20 05:04:08 -0700

    Exercise Complete

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,53 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Schema;
 
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet03
     {
+        
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+           return Array.Exists(vals, element => element != null && element == false);        
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+           
+            if (numbers == null) return false;
+
+            return (numbers.Sum() % 2 != 0);
+            
+            
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            bool hasUpper = password.Any(char.IsUpper);
+            bool hasNumber = password.Any(char.IsNumber);
+            bool hasLower = password.Any(char.IsLower);
+
+            return hasUpper && hasNumber && hasLower;
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[val.Length - 1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if(divisor ==0)
+            {
+                return 0;
+            }
+            return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            int[] oddsArray = new int[50]; 
+            int oddsIndex = 0;
+
+            for (int i = 1; i < 100; i += 2)
+            { 
+                oddsArray[oddsIndex] = i;
+                oddsIndex++;
+            }
+
+            return oddsArray;
+
+
+
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
         }
     }
 }
